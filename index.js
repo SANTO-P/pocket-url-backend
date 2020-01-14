7395edd283d80262727d240df6e83b677dc8884c
SANTO-P <santoshpatil.004@gmail.com>
2020-01-14 08:46:59 +0530

    Changed PORT

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.use(express.json({ extended: false }));
 app.use("/", require("./routes/index"));
 app.use("/api/url", require("./routes/urls"));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
